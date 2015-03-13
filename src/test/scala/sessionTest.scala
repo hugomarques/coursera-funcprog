8239a9921d31f45d5a3d37f3a39a03d8fb4d09fb
Hugo Marques <hugodesmarques@gmail.com>
2015-03-12 22:41:41 -0700

    Include test support

---
+++ src/test/scala/sessionTest.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FunSuite
+
+/**
+ * Created by hugomarques on 12/3/15.
+ */
+class sessionTest extends FunSuite {
+
+  test("sqrt works correctly") {
+    //assert(session.sqrt(4) == 2)
+  }
+
+
+
+}
